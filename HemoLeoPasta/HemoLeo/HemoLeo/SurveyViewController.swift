cb2ccbab70ad46b2c1eec16c92acedbb02e897f8
Gabriel Oliveira <gabriel90939@gmail.com>
2016-07-21 23:23:54 -0300

    Research Kit funcionando direito (FINALMENTE!!)

--- HemoLeoPasta/HemoLeo/HemoLeo/SurveyViewController.swift
+++ HemoLeoPasta/HemoLeo/HemoLeo/SurveyViewController.swift
@@ -10,36 +10,21 @@ import UIKit
 import ResearchKit
 
 class SurveyViewController: ORKTaskViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.frame = CGRectMake(0, 0, 1024, 768)
-        self.delegate = SymptomCardViewController()
-//        self.stepViewController
-        self.view.alignmentRectInsets()
+        self.view.layoutIfNeeded()
 
         // Do any additional setup after loading the view.
     }
-    
-//    override func viewWillAppear(animated: Bool) {
-//        let taskViewController = ORKTaskViewController(task: task, restorationData: nil, delegate: SymptomCardViewController())
-//        
-//        self.presentViewController(taskViewController, animated: true, completion: nil)
-//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        self.view.frame = CGRectMake(-100, -50, 600, 700)
+        self.view.autoresizesSubviews = false
     }
-    */
-
 }
