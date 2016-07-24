f8b1101037fb97926af7b1739dc0749b9a6bca1c
Gabriel Oliveira <gabriel90939@gmail.com>
2016-07-23 23:20:16 -0300

    Research Kit salvando os resultados no Symptom Tracker

--- HemoLeoPasta/HemoLeo/HemoLeo/SurveyViewController.swift
+++ HemoLeoPasta/HemoLeo/HemoLeo/SurveyViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import ResearchKit
+import CareKit
 
 class SurveyViewController: ORKTaskViewController {
+    private let storeManager: CarePlanStoreManager = CarePlanStoreManager.sharedCarePlanStoreManager
+    private var assessmentManager: AssessmentsManager? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
-        self.delegate = SymptomCardViewController()
+        assessmentManager = AssessmentsManager(carePlanStore: storeManager.store)
+        self.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -27,5 +31,43 @@ class SurveyViewController: ORKTaskViewController {
     override func viewDidLayoutSubviews() {
         self.view.frame = CGRectMake(-90, -50, 600, 700)
         self.view.autoresizesSubviews = false
+    }
+}
+
+//MARK: - Task View Controller Delegate
+extension SurveyViewController: ORKTaskViewControllerDelegate {
+    // Called with then user completes a presented `ORKTaskViewController`.
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        defer {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        // Make sure the reason the task controller finished is that it was completed.
+        guard reason == .Completed else { return }
+        
+        // Determine the event that was completed and the `SampleAssessment` it represents.
+        guard let event = SymptomCardViewController.viewController.lastSelectedAssessmentEvent,
+            activityType = ActivityType(rawValue: event.activity.identifier),
+            sampleAssessment = assessmentManager!.activityWithType(activityType) else { return }
+        
+        // Build an `OCKCarePlanEventResult` that can be saved into the `OCKCarePlanStore`.
+        let carePlanResult = sampleAssessment.buildResultForCarePlanEvent(event, taskResult: taskViewController.result)
+        
+        completeEvent(event, inStore: storeManager.store, withResult: carePlanResult)
+    }
+    
+    func taskViewController(taskViewController: ORKTaskViewController, stepViewControllerWillAppear stepViewController: ORKStepViewController) {
+        stepViewController.skipButtonTitle = "Pular"
+        stepViewController.continueButtonTitle = "Próximo"
+        
+    }
+    
+    // MARK: Convenience
+    func completeEvent(event: OCKCarePlanEvent, inStore store: OCKCarePlanStore, withResult result: OCKCarePlanEventResult) {
+        store.updateEvent(event, withResult: result, state: .Completed) { success, _, error in
+            if !success {
+                print(error?.localizedDescription)
+            }
+        }
     }
 }
--- HemoLeoPasta/HemoLeo/HemoLeo/SymptomCardViewController.swift
+++ HemoLeoPasta/HemoLeo/HemoLeo/SymptomCardViewController.swift
@@ -13,7 +13,7 @@ import ResearchKit
 class SymptomCardViewController: UINavigationController {
     private let storeManager: CarePlanStoreManager = CarePlanStoreManager.sharedCarePlanStoreManager
     private var assessmentManager: AssessmentsManager? = nil
-    private var viewController: OCKSymptomTrackerViewController!
+    static var viewController: OCKSymptomTrackerViewController!
     private var task: ORKTask!
     
     override func viewDidLoad() {
@@ -23,8 +23,8 @@ class SymptomCardViewController: UINavigationController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        viewController = createCareCardVC()
-        self.pushViewController(viewController, animated: false)
+        SymptomCardViewController.viewController = createCareCardVC()
+        self.pushViewController(SymptomCardViewController.viewController, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,75 +73,13 @@ extension SymptomCardViewController: OCKSymptomTrackerViewControllerDelegate {
             assessmentEvent.state == .NotCompleted ||
             (assessmentEvent.state == .Completed && assessmentEvent.activity.resultResettable) else { return }
         
-        // Show an `ORKTaskViewController` for the assessment's task.
-//        let taskViewController = ORKTaskViewController(task: sampleAssessment.task(), taskRunUUID: nil)
+        //Show an `ORKTaskViewController` for the assessment's task.
         let taskViewController = self.storyboard?.instantiateViewControllerWithIdentifier("survey") as! SurveyViewController
         
         task = sampleAssessment.task()
-        
         taskViewController.popoverPresentationController?.delegate = self
         
-//        taskViewController.view.frame = CGRectMake(0, 0, 1000, 1000)
-        
-//        let popoverViewController = taskViewController
-//        popoverViewController.modalPresentationStyle = .CurrentContext
-//        
-//        let popoverPresentationViewController = popoverViewController.popoverPresentationController
-//        
-//        popoverPresentationViewController?.permittedArrowDirections = UIPopoverArrowDirection.Up
-//        popoverPresentationViewController?.delegate = self
-//        popoverPresentationViewController?.sourceView = self.view
-//        popoverPresentationViewController?.sourceRect = CGRectMake(500, 900, 100, 100)
-        
-//        ButtonAnimation.addButtonPressAnimationToView(careCardButton)
-        
-//        presentViewController(popoverViewController, animated: true, completion: nil)
-        
         performSegueWithIdentifier("symptomToSurvey", sender: self)
-    }
-}
-
-//MARK: - TaskViewController Delegate
-extension SymptomCardViewController: ORKTaskViewControllerDelegate {
-    // Called with then user completes a presented `ORKTaskViewController`.
-    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
-        defer {
-            dismissViewControllerAnimated(true, completion: nil)
-        }
-        
-        // Make sure the reason the task controller finished is that it was completed.
-        guard reason == .Completed else { return }
-        
-        // Determine the event that was completed and the `SampleAssessment` it represents.
-        guard let event = viewController.lastSelectedAssessmentEvent,
-            activityType = ActivityType(rawValue: event.activity.identifier),
-            sampleAssessment = assessmentManager!.activityWithType(activityType) else { return }
-        
-        // Build an `OCKCarePlanEventResult` that can be saved into the `OCKCarePlanStore`.
-        let carePlanResult = sampleAssessment.buildResultForCarePlanEvent(event, taskResult: taskViewController.result)
-        
-        completeEvent(event, inStore: storeManager.store, withResult: carePlanResult)
-    }
-    
-    
-//    task
-    
-    // MARK: Convenience
-    private func completeEvent(event: OCKCarePlanEvent, inStore store: OCKCarePlanStore, withResult result: OCKCarePlanEventResult) {
-        store.updateEvent(event, withResult: result, state: .Completed) { success, _, error in
-            if !success {
-                print(error?.localizedDescription)
-            }
-        }
-    }
-    
-    func taskViewController(taskViewController: ORKTaskViewController, stepViewControllerWillAppear stepViewController: ORKStepViewController) {
-        stepViewController.skipButtonTitle = "Pular"
-        stepViewController.continueButtonTitle = "Próximo"
-//        stepViewController.automaticallyAdjustsScrollViewInsets = false
-        
-//        stepViewController.preferredContentSize = CGSizeMake(414, 736)
-//        stepViewController.view.ins
     }
 }
 
