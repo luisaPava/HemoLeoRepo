da7ba0fad4d0003c0a7aa7b9bfce59d84b3c99a2
Gabriel Oliveira <gabriel90939@gmail.com>
2016-11-18 17:57:32 -0200

    Pod text

---
+++ HemoLeoPasta/HemoLeo/Pods/Target Support Files/Pods-HemoLeo/Pods-HemoLeo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HemoLeo : NSObject
+@end
+@implementation PodsDummy_Pods_HemoLeo
+@end
---
+++ HemoLeoPasta/HemoLeo/Pods/Target Support Files/Pods-HemoLeo/Pods-HemoLeo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HemoLeoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HemoLeoVersionString[];
+
