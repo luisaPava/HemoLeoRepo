2d0498410fb8fb9deb3882f9d9412630d476bd1a
Luisa Mello <lu_mello93@hotmail.com>
2016-11-18 10:38:18 -0200

    Tutorial

--- HemoLeoPasta/HemoLeo/HemoLeo/Tutorial1ViewController.swift
+++ HemoLeoPasta/HemoLeo/HemoLeo/Tutorial1ViewController.swift
@@ -9,16 +9,22 @@
 import UIKit
 
 class Tutorial1ViewController: UIViewController {
+    @IBOutlet weak var passo2: TutorialPasso2View!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "BG"))
         
         if UIDevice.current.modelName.contains("iPhone") {
             
             
         } else {
             
-            
+            passo2.addPasso2Animation()
+            passo2.clipsToBounds = true
         }
 
         // Do any additional setup after loading the view.
