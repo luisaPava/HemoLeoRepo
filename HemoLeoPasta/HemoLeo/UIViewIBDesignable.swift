54868ee4467a46093e1d8329fc07ba55d15a7f54
Gabriel Oliveira <gabriel90939@gmail.com>
2016-11-20 20:41:59 -0200

    IBDesignables

---
+++ HemoLeoPasta/HemoLeo/UIViewIBDesignable.swift
@@ -0,0 +1,83 @@
+//
+//  UIViewIBDesignable.swift
+//  HemoLeo
+//
+//  Created by Gabriel Oliveira on 20/11/16.
+//  Copyright © 2016 Bepid. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomView: UIView {
+    
+    //MARK: PROPERTIES
+    
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 1.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornurRadius: CGFloat = 1.0 {
+        didSet {
+            layer.cornerRadius = cornurRadius
+            clipsToBounds = true
+        }
+    }
+    
+    //MARK: Initializers
+    override init(frame : CGRect) {
+        super.init(frame : frame)
+        setup()
+        configure()
+    }
+    
+    convenience init() {
+        self.init(frame:CGRect.zero)
+        setup()
+        configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+        configure()
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+        configure()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setup()
+        configure()
+    }
+    
+    func setup() {
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 1.0
+    }
+    
+    func configure() {
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+        layer.cornerRadius = cornurRadius
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+}
+
