e9a17fe5352315e229263f7916134562d6b864aa
Gabriel Oliveira <gabriel90939@gmail.com>
2016-11-24 21:51:43 -0200

    Notificações

--- HemoLeoPasta/HemoLeo/NotificationsModel.swift
+++ HemoLeoPasta/HemoLeo/NotificationsModel.swift
@@ -7,8 +7,60 @@
 //
 
 import Foundation
+import UserNotifications
 
 class NotificationsModel {
+    private var center = UNUserNotificationCenter.current()
+    static let sharedNotificationsModel = NotificationsModel()
     
+    private init() {
+        registerLocal()
+    }
+    
+    private func registerLocal() {
+        center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+            if !granted {
+                
+            }
+        })
+    }
+
+    func createNotification(title: String, body: String, weekDay: Int) {
+        let content = UNMutableNotificationContent()
+        let calendar = Calendar.current
+        let date = getDate(withWeekDay: weekDay)
+        
+        let alarmTime = date.addingTimeInterval(60 * 60 * 24 * 7 - 300)
+        let components = calendar.dateComponents([.weekday, .hour, .minute], from: alarmTime)
+        
+        content.title = title
+        content.body = body
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error?.localizedDescription as Any)
+            }
+        })
+    }
+    
+    private func getDate(withWeekDay day: Int) -> Date {
+        let today = Date()
+        let daysToAdd: Int!
+        let calendar = Calendar.current
+        let weekday = calendar.component(.weekday, from: today)
+        
+        if weekday > day {
+            daysToAdd = (7 - weekday) + day
+        } else {
+            daysToAdd = (day - weekday)
+        }
+        
+        let newDate = calendar.date(byAdding: .weekday, value: daysToAdd, to: today)
+        
+        return newDate!
+    }
 }
 
